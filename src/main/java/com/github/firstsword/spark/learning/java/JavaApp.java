fdc0b6849bacc0b30b9358183c70e2d0f76a6fcd
马杨 <yang.ma@eoitek.com>
2019-11-01 17:06:43 +0800

    init proj

---
+++ src/main/java/com/github/firstsword/spark/learning/java/JavaApp.java
@@ -0,0 +1,7 @@
+package com.github.firstsword.spark.learning.java;
+
+public class JavaApp {
+    public static void main(String[] args) {
+        System.out.println("hello, java");
+    }
+}
---
+++ src/main/scala/com/github/firstsword/spark/learning/scala/ScalaApp.scala
@@ -0,0 +1,7 @@
+package com.github.firstsword.spark.learning.scala
+
+object ScalaApp {
+  def main(args: Array[String]): Unit = {
+    println("hello, scala")
+  }
+}
